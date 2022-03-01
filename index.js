bde5be8fd0437c1d6172dd8370470b7f5940fbec
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-01 15:49:40 -0800

    commit tree f68fb2acc2e8b6c6cb006cc85172fd345c0b2f33

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+   if (block > 42) 
+   return block - 42;
+   else return  42 - block;
+}
+
+function distanceFromHqInFeet(block) {
+   return distanceFromHqInBlocks(block) * 264;   
+}
+
+function distanceTravelledInFeet(start, destination){
+    const block = 264
+    if(start < destination){
+        return (destination - start) * 264; 
+    } else { 
+        return (start - destination) * 264; 
+}}
+
+ 
+function calculatesFarePrice(start, destination) {
+   const travelledInFeet = distanceTravelledInFeet(start, destination);
+    if (travelledInFeet <= 400) 
+    {
+    return (0)
+    }
+    else if (travelledInFeet > 400 && travelledInFeet <= 2000)
+    {
+        return (travelledInFeet -400) * .02;
+    }
+    else if (travelledInFeet > 2000 && travelledInFeet <=2500)
+        {
+        return (25);
+    }
+    else if (travelledInFeet > 2500)
+    {
+        return ('cannot travel that far')
+    }}
+    
